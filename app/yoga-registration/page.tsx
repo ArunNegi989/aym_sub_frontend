2ee013a272f1e4619bd3444bd0a994b6a62214d4
Gagan <gagandhyani17@gmail.com>
2026-07-15 10:51:18 +0530

    created seprate PageClient files for frontend and seo on page.tsx

--- app/yoga-registration/page.tsx
+++ app/yoga-registration/page.tsx
@@ -1,5 +1,15 @@
 import { Suspense } from "react";
 import RegisterForm from "./RegisterForm";
+import { Metadata } from "next";
+
+
+
+export const metadata: Metadata = {
+  title: "Yoga Registration | Enroll at AYM Yoga School",
+  description:
+    "Complete your yoga registration at AYM Yoga School. Secure your seat for yoga teacher training, retreats, and wellness programs in Rishikesh.",
+};
+
 
 export default function Page() {
   return (
